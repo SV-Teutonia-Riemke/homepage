683c604f009529daa128be7b8e14dafc4c362bec
Nikita Loges <git@loges.one>
2023-08-25 22:41:03 +0200

    update to latest tabler version

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,6 +1,11 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
-import Clipboard from 'stimulus-clipboard'
-import '@tabler/core'
+import Clipboard from 'stimulus-clipboard';
+import '@tabler/core';
+import Iconify from '@iconify/iconify';
+
+Iconify.addAPIProvider('', {
+    resources: [],
+});
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 const app = startStimulusApp(require.context(
