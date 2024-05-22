6974185af014bf7b930b7802bf77458c1676a4b4
Nikita Loges <git@loges.one>
2024-05-22 21:47:12 +0200

    fix config env

---
+++ config/packages/test/web_profiler.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Config\FrameworkConfig;
+use Symfony\Config\WebProfilerConfig;
+
+return static function (
+    WebProfilerConfig $webProfilerConfig,
+    FrameworkConfig $frameworkConfig,
+): void {
+    $webProfilerConfig
+        ->toolbar(false)
+        ->interceptRedirects(false);
+
+    $frameworkConfig->profiler()
+        ->collect(false);
+};
