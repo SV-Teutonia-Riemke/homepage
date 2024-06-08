2669f264bd74e7ab660048a7d66a0a646e6d3295
Nikita Loges <git@loges.one>
2024-06-08 19:39:05 +0200

    fix bundles

--- config/bundles.php
+++ config/bundles.php
@@ -1,32 +1,63 @@
 <?php
 
+declare(strict_types=1);
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
+use Eckinox\TinymceBundle\TinymceBundle;
+use HWI\Bundle\OAuthBundle\HWIOAuthBundle;
+use Knp\Bundle\MenuBundle\KnpMenuBundle;
+use Knp\Bundle\PaginatorBundle\KnpPaginatorBundle;
+use Liip\ImagineBundle\LiipImagineBundle;
+use Misd\PhoneNumberBundle\MisdPhoneNumberBundle;
+use Oneup\FlysystemBundle\OneupFlysystemBundle;
+use Pentatrion\ViteBundle\PentatrionViteBundle;
+use Presta\SitemapBundle\PrestaSitemapBundle;
+use Sentry\SentryBundle\SentryBundle;
+use Shapecode\Bundle\TwigStringLoaderBundle\ShapecodeTwigStringLoaderBundle;
+use Spiriit\Bundle\FormFilterBundle\SpiriitFormFilterBundle;
+use Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
+use Symfony\UX\Autocomplete\AutocompleteBundle;
+use Symfony\UX\Dropzone\DropzoneBundle;
+use Symfony\UX\Icons\UXIconsBundle;
+use Symfony\UX\LiveComponent\LiveComponentBundle;
+use Symfony\UX\StimulusBundle\StimulusBundle;
+use Symfony\UX\Turbo\TurboBundle;
+use Symfony\UX\TwigComponent\TwigComponentBundle;
+use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
+
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
-    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
-    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
-    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
-    Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
-    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
-    Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class => ['all' => true],
-    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Spiriit\Bundle\FormFilterBundle\SpiriitFormFilterBundle::class => ['all' => true],
-    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
-    Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
-    Shapecode\Bundle\TwigStringLoaderBundle\ShapecodeTwigStringLoaderBundle::class => ['all' => true],
-    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
-    Eckinox\TinymceBundle\TinymceBundle::class => ['all' => true],
-    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
-    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
+    FrameworkBundle::class => ['all' => true],
+    MonologBundle::class => ['all' => true],
+    DoctrineBundle::class => ['all' => true],
+    DoctrineMigrationsBundle::class => ['all' => true],
+    TwigBundle::class => ['all' => true],
+    TwigExtraBundle::class => ['all' => true],
+    SecurityBundle::class => ['all' => true],
+    WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    OneupFlysystemBundle::class => ['all' => true],
+    KnpPaginatorBundle::class => ['all' => true],
+    TwigComponentBundle::class => ['all' => true],
+    TurboBundle::class => ['all' => true],
+    KnpMenuBundle::class => ['all' => true],
+    LiveComponentBundle::class => ['all' => true],
+    AutocompleteBundle::class => ['all' => true],
+    DropzoneBundle::class => ['all' => true],
+    LiipImagineBundle::class => ['all' => true],
+    MisdPhoneNumberBundle::class => ['all' => true],
+    SentryBundle::class => ['all' => true],
+    StofDoctrineExtensionsBundle::class => ['all' => true],
+    SpiriitFormFilterBundle::class => ['all' => true],
+    StimulusBundle::class => ['all' => true],
+    PrestaSitemapBundle::class => ['all' => true],
+    ShapecodeTwigStringLoaderBundle::class => ['all' => true],
+    UXIconsBundle::class => ['all' => true],
+    TinymceBundle::class => ['all' => true],
+    HWIOAuthBundle::class => ['all' => true],
+    PentatrionViteBundle::class => ['all' => true],
 ];
