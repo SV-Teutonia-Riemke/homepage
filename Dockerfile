14448ad6530cf57fda852a3be5b333d6b632086f
Nikita Loges <git@loges.one>
2025-11-09 12:45:00 +0100

    docker setup

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,21 @@ ENV APP_USER="app"
 ENV APP_GROUP="app"
 ENV APP_UID="1000"
 ENV APP_GID="1000"
+ENV APP_PATH="/app"
+ENV ENABLE_SSHD=0
+ENV DISABLE_AUTORUN_COMPOSER_INSTALL=1
+ENV COMPOSER_OPTIMIZE_AUTOLOADER=0
+ENV TERM=xterm-256color
+ENV COLORTERM=truecolor
 
-COPY --chown=$APP_USER:$APP_GROUP . /var/www/html/
+RUN rm /etc/hooks/bootstrap/10-create-project && \
+    rm /etc/hooks/bootstrap/20-create-index && \
+    rm /etc/hooks/bootstrap/30-other-fixes
+
+COPY --chown=$APP_USER:$APP_GROUP . /app
 COPY ./Caddyfile /etc/caddy/Caddyfile
 COPY ./etc/startup/00-startup /startup/00-startup
 
 RUN chmod +x /startup/00-startup
+
+WORKDIR /app
---
+++ etc/tools/rector.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+
+$projectDir = dirname(__DIR__, 2);
+
+return RectorConfig::configure()
+    ->withPaths([
+        $projectDir . '/config',
+        $projectDir . '/public',
+        $projectDir . '/src',
+        $projectDir . '/tests',
+    ])
+    ->withPHPStanConfigs([
+        __DIR__ . '/phpstan.neon',
+    ])
+    ->withCache($projectDir . '/var/cache/rector', FileCacheStorage::class)
+    ->withSymfonyContainerXml($projectDir . '/var/cache/test/App_KernelTestDebugContainer.xml')
+    ->withPhpSets()
+    ->withAttributesSets(
+        symfony: true,
+        doctrine: true,
+    )
+    ->withComposerBased(
+        doctrine: true,
+        phpunit: true,
+        symfony: true,
+    )
+    ->withTypeCoverageLevel(50)
+    ->withDeadCodeLevel(50)
+    ->withCodeQualityLevel(50)
+    ->withImportNames(removeUnusedImports: true)
+    ->withSets([
+        $projectDir . '/vendor/thecodingmachine/safe/rector-migrate.php',
+    ])
+    ->withRules([
+//        StringExtensionToConfigBuilderRector::class,
+    ]);
